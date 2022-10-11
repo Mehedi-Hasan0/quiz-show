68e126fba82f0d3d0a144b0022f0b167f1bcaa97
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-10-11 23:45:44 +0600

    quiz card added in main section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,12 +14,16 @@ const Home = () => {
             </header>
 
             <main>
-                {
-                    quizData.data.map(quiz => <MainBody
-                        key={quiz.id}
-                        quiz={quiz}
-                    ></MainBody>)
-                }
+                <p className='font-poppins text-xl text-[#2370C8] font-medium'>TOP QUIZ</p>
+                <h1 className='font-poppins text-4xl sm:text-5xl font-semibold text-[#262A4E]'>Test Your Knowledge!</h1>
+                <div className='flex sm:flex-row sm:justify-around md:flex-wrap lg:flex-wrap lg:my-3 md:my-3 justify-between flex-col w-11/12 mx-auto my-4'>
+                    {
+                        quizData.data.map(quiz => <MainBody
+                            key={quiz.id}
+                            quiz={quiz}
+                        ></MainBody>)
+                    }
+                </div>
             </main>
             <footer>
                 <Footer></Footer>
--- src/components/MainBody/MainBody.js
+++ src/components/MainBody/MainBody.js
@@ -3,8 +3,16 @@ import React from 'react';
 const MainBody = ({ quiz }) => {
     const { name, logo } = quiz;
     return (
-        <div>
-
+        <div className='flex flex-col bg-white p-2 shadow-lg items-center max-w-xs mt-4
+        mb-16 mx-auto'>
+            <img src={logo} alt="logo" className='w-[220px] h-[220px] bg-slate-200' />
+            <div className='flex flex-row justify-between items-center py-2 w-full'>
+                <p className='font-poppins'>{name}</p>
+                <p className='font-poppins'>Q:</p>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded font-poppins text-sm">
+                    Get Started
+                </button>
+            </div>
         </div>
     );
 };
