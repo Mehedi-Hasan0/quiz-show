ee49d5eb0773cf26558d51c8cc8e30fb50bf963b
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-10-12 21:01:21 +0600

    toast added

--- src/components/QuizCard/QuizCard.js
+++ src/components/QuizCard/QuizCard.js
@@ -2,9 +2,8 @@ import React from 'react';
 import QuizOption from './QuizOption/QuizOption';
 
 const QuizCard = ({ quizData }) => {
-    const { id, options, question, correctAnswer } = quizData;
+    const { options, question, correctAnswer } = quizData;
 
-    let num = 0;
     return (
         <div className=' p-5 my-8 shadow-lg bg-blue-50 sm:max-w-3xl w-10/12 mx-auto'>
             <h2 className='font-poppins font-medium leading-7 text-gray-600 text-2xl mb-6'>Quiz:{question}</h2>
@@ -12,6 +11,7 @@ const QuizCard = ({ quizData }) => {
                 {
                     options.map(option => <QuizOption
                         option={option}
+                        correctAnswer={correctAnswer}
                     ></QuizOption>)
                 }
             </div>
